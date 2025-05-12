f7ce784b35ac13837920478a605071e043dbb0e7
Ketan Mujumdar <ketanmujumdar@gmail.com>
2025-05-12 11:54:45 +0530

    demo

--- main.go
+++ main.go
@@ -4,18 +4,18 @@ import "fmt"
 
 // calculateSum calculates the sum of a slice of numbers
 func calculateSum(numbers []int) int {
-    sum := 0
-    for _, num := range numbers {
-        sum += num
-    }
-    return sum
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
 }
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
 
-    // Demonstrate the new feature
-    numbers := []int{1, 2, 3, 4, 5}
-    sum := calculateSum(numbers)
-    fmt.Printf("The sum of numbers %v is: %d\n", numbers, sum)
+	// Demonstrate the new feature
+	numbers := []int{1, 2, 3, 4, 5}
+	sum := calculateSum(numbers)
+	fmt.Printf("The sum of numbers %v is: %d\n", numbers, sum)
 }
